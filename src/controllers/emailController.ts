c14da19aca904c60cba0d56d5b435b8cc5b1612b
Carlos Eduardo <carlosedbat@gmail.com>
2022-06-20 20:48:20 -0300

    Project finished. Route post has been configured on '/contato' for sending emails with camps referred in the readme.md

--- src/controllers/emailController.ts
+++ src/controllers/emailController.ts
@@ -1,9 +1,36 @@
 import {Request, Response} from 'express'
+import nodemailer from 'nodemailer';
+import { text } from 'stream/consumers';
+import dotenv from 'dotenv'
 
-export const contato = (req:Request, res:Response)=>{
-    
+dotenv.config();
 
+export const contato = async (req:Request, res:Response)=>{
+    //Passo 1: Configurar o transporter
+    let transport = nodemailer.createTransport({
+        host: process.env.MailHost,
+        port: parseInt(process.env.MailPort as string),
+        auth: {
+          user: process.env.MailUser,
+          pass: process.env.MailPass
+        }
+      });
+    //Passo 2: Configurar a mensagem
 
-    res.json({pong:true});
+    let message = {
+        from: 'nao-responda@dominio.com.br>',
+        to:'suporte@dominio.com.br',
+        replyTo: req.body.from ,
+        subject:req.body.subject,
+        html: req.body.email,
+        text: req.body.email
+    }
+    //Passo 3: Enviar a mensagem
+
+    let info = await transport.sendMail(message);
+
+    console.log("INFO", info);
+
+    res.json({sucess:true});
 };
 
